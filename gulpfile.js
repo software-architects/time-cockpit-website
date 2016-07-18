622fa421dde23fffa36c23792ca9060a971345e0
Karin Huber <karin@timecockpit.com>
2016-07-18 08:22:28 +0200

    Add bootstrap-sass

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require("gulp");
+
+gulp.task("default", ["copyBootstrapFiles"], function () {
+    // place code for your default task here
+});
+
+gulp.task("copyBootstrapFiles", function () {
+	gulp.src("./node_modules/bootstrap-sass/assets/stylesheets/**/*.*")
+		.pipe(gulp.dest("./_sass/bootstrap"));
+});
