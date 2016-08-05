b6ff6c7d979f1de7e51b14b5b5c0099aad5a6ca3
Jovan <jovan@timecockpit.com>
2016-08-05 07:51:15 +0200

    -

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var uglify = require("gulp-uglify");
 var pump = require("pump");
 var stripBom = require("gulp-stripbom");
 
-gulp.task("default", ["copyBootstrapFiles","buildTypescript"], function () {
+gulp.task("default", ["copyBootstrapFiles","buildTypescript","images","compress","removeBom"], function () {
     // place code for your default task here
 });
 
@@ -30,6 +30,7 @@ gulp.task("buildTypescript", function () {
         .pipe(gulp.dest("scripts"));
 });
 
+//Compress all images
 gulp.task("images", function () {
     var allFilter = filter(["**/*.jpeg", "**/*.gif", "**/*.jpg", "**/*.png"], { restore: true });
 
@@ -41,6 +42,7 @@ gulp.task("images", function () {
         .pipe(gulp.dest("content/images"))
 });
 
+//Compress all javascript files
 gulp.task("compress", function (cb) {
     pump([
           gulp.src("'scripts/**/*"),
@@ -51,6 +53,7 @@ gulp.task("compress", function (cb) {
     );
 });
 
+//Remove the byte order mark from all files
 gulp.task("removeBom", function () {
 
     return gulp.src("**!(.sln)/*!(.sln)")
